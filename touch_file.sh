c91612314a6ce7e41d0906c7cb74848613661bba
root <root@eagle-soar.com>
2013-09-05 10:24:57 +0800

    save md5 file

--- touch_file.sh
+++ touch_file.sh
@@ -3,14 +3,25 @@
 
 FILE_LIST="./list.log"
 
+# file Checker
+
+Checker="/usr/bin/md5sum"
+
+
 [ ! -f $FILE_LIST ] && echo "No search $FILE_LIST" && exit 2
 
 
-for File in $(cat $FILE_LIST)
-do
-  if [ -a $File ];then
-    echo "OK is $File" >> ./tmp.log
-  else
-    echo "$File" >> ./tmp.log
-  fi
-done
+CHECK_FILE(){
+
+  for File in $(cat $FILE_LIST)
+  do
+    if [ -a $File ];then
+      echo "OK is $File" >> ./tmp.log
+    else
+      echo "$File" >> ./tmp.log
+    fi
+  done
+}
+
+
+
